4815d11d0c2840861931bead23a9ca99fedb4255
Miika Alikirri <nykseli@protonmail.com>
2024-07-22 22:58:18 +0300

    server: serve js files from resources

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_files as fs;
+use actix_web::{get, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
 use askama::Template;
 
 #[derive(Template)]
@@ -13,9 +14,17 @@ async fn hello() -> impl Responder {
     HttpResponse::Ok().body(hello)
 }
 
+#[get("/js/{filename:.*.js}")]
+async fn js_files(req: HttpRequest) -> Result<fs::NamedFile, Error> {
+    let path: String = req.match_info().query("filename").parse().unwrap();
+    let path = format!("./resources/js/{path}");
+    let file = fs::NamedFile::open(path)?;
+    Ok(file.use_last_modified(true))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
+    HttpServer::new(|| App::new().service(hello).service(js_files))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
