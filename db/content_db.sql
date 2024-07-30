ceac39beb2fefa92e223dc669fdbd387dc82558a
Miika Alikirri <nykseli@protonmail.com>
2024-07-30 21:54:35 +0300

    db: add content title, created and expires columns

--- db/content_db.sql
+++ db/content_db.sql
@@ -3,6 +3,12 @@ CREATE TABLE content (
     user_id INTEGER NOT NULL,
     -- unique content id
     content_id TEXT UNIQUE NOT NULL,
+    -- when content is created
+    created DATETIME NOT NULL,
+    -- when content should expire, NULL if never
+    expires DATETIME,
+    -- content page title
+    title TEXT UNIQUE NOT NULL,
     -- raw plaintext content
     content TEXT NOT NULL
 );
